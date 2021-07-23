22c1d90d66b6ae99531e74da4fd9e4954c2dfff2
Michael Shilman <michael@lab80.co>
2021-07-23 13:07:30 +0800

    Clear out boilerplate

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -4,13 +4,6 @@ import { Button } from "./Button";
 export default {
   title: "Example/Button",
   component: Button,
-  parameters: {
-    myAddonParameter: `
-<MyComponent boolProp scalarProp={1} complexProp={{ foo: 1, bar: '2' }}>
-  <SomeOtherComponent funcProp={(a) => a.id} />
-</MyComponent>
-`,
-  },
 };
 
 const Template = (args) => <Button {...args} />;
