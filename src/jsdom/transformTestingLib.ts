532a08a98152b344bfc889a0246bf099c39ce48b
Yann Braga <yannbf@gmail.com>
2021-12-16 15:43:17 +0100

    fix broken jsdom runner

--- src/jsdom/transformTestingLib.ts
+++ src/jsdom/transformTestingLib.ts
@@ -8,6 +8,7 @@ const filePrefixer = template(`
 
 const testPrefixer = template(
   `
+    console.log({ id: %%id%%, title: %%title%%, name: %%name%%, storyExport: %%storyExport%%, hasPlayFn: %%hasPlayFn%%});
     async () => {
       const Composed = await composeStory(%%storyExport%%, exports.default);
       const { container } = render(<Composed />);
@@ -22,10 +23,10 @@ const testPrefixer = template(
 );
 
 export const transformTestingLib = (src: string) => {
-  const start = new Date();
+  // const start = new Date();
   // @ts-ignore
   const result = transformCsf(src, { filePrefixer, testPrefixer, insertTestIfEmpty: true });
   // @ts-ignore
-  console.log('transform', new Date() - start);
+  // console.log('transform', new Date() - start);
   return result;
 };
