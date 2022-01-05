aadb10a2742efdcba45085bc2af05bc3678d85eb
Yann Braga <yannbf@gmail.com>
2022-01-05 15:57:43 +0100

    define root directory for out of box config

--- playwright-jest.config.js
+++ playwright-jest.config.js
@@ -1,18 +1,13 @@
 module.exports = {
   cacheDirectory: '.cache/jest',
+  rootDir: process.cwd(),
   testMatch: ['**/*.stories.[jt]s?(x)'],
-  moduleNameMapper: {
-    // non-js files
-    '\\.(jpg|jpeg|png|apng|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/mocks/fileMock.js',
-    '\\.(css|scss|stylesheet)$': '<rootDir>/mocks/styleMock.js',
-  },
   transform: {
-    '^.+\\.stories\\.[jt]sx?$': './playwright/transform',
+    '^.+\\.stories\\.[jt]sx?$': '@storybook/test-runner/playwright/transform',
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
   preset: 'jest-playwright-preset',
-  globalSetup: './playwright/global-setup.js',
-  globalTeardown: './playwright/global-teardown.js',
-  testEnvironment: './playwright/custom-environment.js',
+  globalSetup: '@storybook/test-runner/playwright/global-setup.js',
+  globalTeardown: '@storybook/test-runner/playwright/global-teardown.js',
+  testEnvironment: '@storybook/test-runner/playwright/custom-environment.js',
 };
