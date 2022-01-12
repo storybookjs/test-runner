77a1b4bda4c35c9b53b462fbc54e0148e2978e32
Yann Braga <yannbf@gmail.com>
2022-01-12 12:26:03 +0100

    chore: add addon-interactions to example

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,8 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
 };
