7eac123c56d71106e42a3fe780f363e7e0e77b6c
Yann Braga <yannbf@gmail.com>
2022-02-09 16:19:30 +0100

    fix typo in default config

--- playwright/test-runner-jest.config.js
+++ playwright/test-runner-jest.config.js
@@ -1,7 +1,7 @@
-const { getJestConfig } = require('@storybook/playwright');
+const { getJestConfig } = require('@storybook/test-runner');
 
 module.exports = {
-  // The default configuration comes from @storybook/playwright
+  // The default configuration comes from @storybook/test-runner
   ...getJestConfig()
   /** Add your own overrides below
    * @see https://jestjs.io/docs/configuration
