ac13f7b463b00dda5f09af093a4c11c0cda71068
Yann Braga <yannbf@gmail.com>
2022-06-13 19:21:36 +0200

    fix: prefix coverage environment variable with storybook

--- bin/test-storybook.js
+++ bin/test-storybook.js
@@ -176,7 +176,7 @@ const main = async () => {
   process.env.TARGET_URL = targetURL;
 
   if (runnerOptions.coverage) {
-    process.env.COLLECT_COVERAGE = 'true';
+    process.env.STORYBOOK_COLLECT_COVERAGE = 'true';
   }
 
   if (process.env.REFERENCE_URL) {
@@ -216,7 +216,7 @@ const main = async () => {
 
   await executeJestPlaywright(jestOptions);
 
-  if (process.env.COLLECT_COVERAGE === 'true') {
+  if (process.env.STORYBOOK_COLLECT_COVERAGE === 'true') {
     printCoverageReport();
   }
 };
