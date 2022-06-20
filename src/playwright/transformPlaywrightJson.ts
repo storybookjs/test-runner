f438500ab2303de5a96e1f7fd1f92132175465a7
Tom Coleman <tom@chromatic.com>
2022-06-20 21:05:44 +1000

    Handle v3 & v4 indexes in transform

--- src/playwright/transformPlaywrightJson.ts
+++ src/playwright/transformPlaywrightJson.ts
@@ -1,18 +1,16 @@
 import * as t from '@babel/types';
 import generate from '@babel/generator';
-import { toId } from '@storybook/csf';
+import { ComponentTitle, StoryId, StoryName, toId } from '@storybook/csf';
 
 import { testPrefixer } from './transformPlaywright';
 
-type Story = { id: string; name: string; title: string; parameters?: Record<string, any> };
-
-const makeTest = (story: Story): t.Statement => {
+const makeTest = (entry: V4Entry): t.Statement => {
   const result: any = testPrefixer({
-    name: t.stringLiteral(story.name),
-    title: t.stringLiteral(story.title),
-    id: t.stringLiteral(story.id),
+    name: t.stringLiteral(entry.name),
+    title: t.stringLiteral(entry.title),
+    id: t.stringLiteral(entry.id),
     // FIXME
-    storyExport: t.identifier(story.id),
+    storyExport: t.identifier(entry.id),
   });
   const stmt = result[1] as t.ExpressionStatement;
   return t.expressionStatement(
@@ -29,28 +27,65 @@ const makeDescribe = (title: string, stmts: t.Statement[]) => {
   );
 };
 
-// FIXME: this should be a CSF helper
-const isDocsOnly = (stories: Story[]) => stories.length === 1 && stories[0].name === 'Page';
+type V4Entry = { type?: 'story' | 'docs'; id: StoryId; name: StoryName; title: ComponentTitle };
+type V4Index = {
+  v: 4;
+  entries: Record<StoryId, V4Entry>;
+};
+
+type V3Story = Omit<V4Entry, 'type'> & { parameters?: Record<string, any> };
+type V3StoriesIndex = {
+  v: 3;
+  stories: Record<StoryId, V3Story>;
+};
+const isV3DocsOnly = (stories: V3Story[]) => stories.length === 1 && stories[0].name === 'Page';
+
+function v3TitleMapToV4TitleMap(titleIdToStories: Record<string, V3Story[]>) {
+  return Object.fromEntries(
+    Object.entries(titleIdToStories).map(([id, stories]) => [
+      id,
+      stories.map(
+        ({ parameters, ...story }) =>
+          ({
+            type: isV3DocsOnly(stories) ? 'docs' : 'story',
+            ...story,
+          } as V4Entry)
+      ),
+    ])
+  );
+}
+
+function groupByTitleId<T extends { title: ComponentTitle }>(entries: T[]) {
+  return entries.reduce((acc, entry) => {
+    const titleId = toId(entry.title);
+    acc[titleId] = acc[titleId] || [];
+    acc[titleId].push(entry);
+    return acc;
+  }, {} as { [key: string]: T[] });
+}
 
 /**
  * Generate one test file per component so that Jest can
  * run them in parallel.
  */
 export const transformPlaywrightJson = (src: string) => {
-  const json = JSON.parse(src);
-  if (json.v !== 3) {
-    throw new Error(`Unsupported version ${json.v}`);
-  }
-  const stories = Object.values(json.stories) as Story[];
-  const titleIdToStories = stories.reduce((acc, story) => {
-    const titleId = toId(story.title);
-    acc[titleId] = acc[titleId] || [];
-    acc[titleId].push(story);
-    return acc;
-  }, {} as { [key: string]: Story[] });
+  const index = JSON.parse(src);
 
-  const titleIdToTest = Object.entries(titleIdToStories).reduce((acc, [titleId, stories]) => {
-    if (!isDocsOnly(stories)) {
+  let titleIdToEntries: Record<string, V4Entry[]>;
+  if (index.v === 3) {
+    const titleIdToStories = groupByTitleId<V3Story>(
+      Object.values((index as V3StoriesIndex).stories)
+    );
+    titleIdToEntries = v3TitleMapToV4TitleMap(titleIdToStories);
+  } else if (index.v === 4) {
+    titleIdToEntries = groupByTitleId<V4Entry>(Object.values((index as V4Index).entries));
+  } else {
+    throw new Error(`Unsupported version ${index.v}`);
+  }
+
+  const titleIdToTest = Object.entries(titleIdToEntries).reduce((acc, [titleId, entries]) => {
+    const stories = entries.filter((s) => s.type !== 'docs');
+    if (stories.length) {
       const storyTests = stories.map((story) => makeDescribe(story.name, [makeTest(story)]));
       const program = t.program([makeDescribe(stories[0].title, storyTests)]);
 
