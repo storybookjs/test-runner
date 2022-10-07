2a0c55589dae8db707e96890805a2665d53d318b
Yann Braga <yannbf@gmail.com>
2022-10-07 11:25:07 +0200

    undo exports map and simplify is-test-runner

--- is-test-runner.cjs
+++
@@ -1,8 +0,0 @@
-/**
- * Returns whether the story is rendering inside of the Storybook test runner.
- */
-module.exports = {
-  isTestRunner: function () {
-    return process?.env?.STORYBOOK_TEST_RUNNER === 'true';
-  }
-}
---
+++ is-test-runner.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
+exports.isTestRunner = isTestRunner;
+
+/**
+ * Returns whether the story is rendering inside of the Storybook test runner.
+ */
+function isTestRunner() {
+  const isTestRunnerInNode = process?.env?.STORYBOOK_TEST_RUNNER === 'true';
+  const isTestRunnerInBrowser = !!(
+    typeof window !== 'undefined' &&
+    window &&
+    window.navigator.userAgent.match(/StorybookTestRunner/)
+  );
+  return isTestRunnerInBrowser || isTestRunnerInNode;
+}
