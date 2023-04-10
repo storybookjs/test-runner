251cd3d6b560c924dc6863b8753e2af0b797be63
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2023-04-10 11:19:38 -0500

    made build output modification easier

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,11 @@ export default defineConfig([
     clean: true,
     entry: ['./src/**/!(*.{d,test}).{js,jsx,ts,tsx}'],
     format: ['cjs', 'esm'],
+    splitting: false,
     dts: true,
+    minify: false,
+    treeshake: false,
+    bundle: false,
     esbuildOptions(options, context) {
       options.platform = 'node';
     },
