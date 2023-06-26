fdfd80a521673d6db6a8470ab87a431385474c78
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2023-06-26 09:48:52 -0500

    fix: Change pkg-dir version

--- src/test-storybook.ts
+++ src/test-storybook.ts
@@ -17,7 +17,7 @@ import { transformPlaywrightJson } from './playwright/transformPlaywrightJson';
 
 import { glob } from 'glob';
 import readPackageUp, { NormalizedReadResult } from 'read-pkg-up';
-import { packageDirectory } from 'pkg-dir';
+import packageDirectory from 'pkg-dir';
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
