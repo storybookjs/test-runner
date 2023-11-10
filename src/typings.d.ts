c7b4beda2afeceddd8eb519846aab96efd4d6613
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2023-11-10 16:52:23 -0500

    feat(typings): Remove unused __getContext variable
    The __getContext variable was no longer being used in the codebase, so it was removed to improve code cleanliness and maintainability.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -4,7 +4,6 @@ import type { StoryContext, StoryIdentifiers } from '@storybook/csf';
 declare global {
   var __sbPreRender: TestHook;
   var __sbPostRender: TestHook;
-  var __getContext: (storyId: string) => any;
   var __getContext: (storyId: string) => StoryContext | StoryIdentifiers;
   var __sbSetupPage: typeof setupPage;
   var __sbCollectCoverage: boolean;
