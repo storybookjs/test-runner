86c01ae17350f5e89b2869d3fc6246c9c69f3508
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2023-11-10 19:47:20 -0500

    chore: Update package.json, src/index.ts, and yarn.lock
    This commit updates the package.json, src/index.ts, and yarn.lock files. The changes were made to keep the project up to date with the latest dependencies and ensure compatibility with other packages.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
+import type { Config } from '@jest/types';
+
 export * from './playwright/hooks';
 export * from './config/jest-playwright';
 export * from './setup-page';
 export * from './util/getTestRunnerConfig';
+
+export {
+  toMatchImageSnapshot,
+  configureToMatchImageSnapshot,
+  updateSnapshotState,
+} from 'jest-image-snapshot';
+
+type JestConfig = Config.InitialOptions;
+type GlobalConfig = Config.GlobalConfig;
+
+export { type JestConfig, type GlobalConfig };
+
+export {
+  injectAxe,
+  configureAxe,
+  getAxeResults,
+  getViolations,
+  reportViolations,
+  checkA11y,
+  DefaultTerminalReporter,
+} from 'axe-playwright';
