2094e4baae4e5859b1f6a7c3f0c45e728b9752ea
Norbert de Langen <ndelangen@me.com>
2025-06-19 09:33:11 +0200

    Update tsup configuration to include Storybook dependencies and adjust external modules

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,24 @@
+import { readFileSync } from 'node:fs';
 import { defineConfig } from 'tsup';
+
+const corePkgJson = JSON.parse(readFileSync('./node_modules/storybook/package.json', 'utf8'));
 
 export default defineConfig([
   {
     entry: ['./src/index.ts', './src/test-storybook.ts'],
     format: ['cjs', 'esm'],
     splitting: false,
+    noExternal: ['storybook/internal/common'],
     dts: true,
     minify: false,
     treeshake: false,
     bundle: true,
     platform: 'node',
-    external: ['@storybook/test-runner'],
+    external: [
+      '@storybook/test-runner',
+      ...Object.keys(corePkgJson.dependencies),
+      ...Object.keys(corePkgJson.peerDependencies),
+    ],
     esbuildOptions(options) {
       options.platform = 'node';
     },
