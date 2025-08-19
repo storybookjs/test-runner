66a721c991c1bd5f299b649f7ffdad8b3535c7cb
matt-halliday <m.halliday@elsevier.com>
2025-08-19 09:36:55 +0100

    docs: add docblock

--- src/config/jest-filename-sequencer.ts
+++ src/config/jest-filename-sequencer.ts
@@ -6,6 +6,11 @@ const sortByFilename = (tests: Array<Test>): Array<Test> => {
   return tests.sort((a, b) => basename(a.path).localeCompare(basename(b.path)));
 };
 
+/**
+ * Custom Jest Test Sequencer that sorts tests by their filenames.
+ * This ensures consistent test execution when using sharding
+ * against an externally deployed Storybook instance using --url.
+ */
 class FilenameSortedTestSequencer extends Sequencer {
   shard(tests: Array<Test>, { shardIndex, shardCount }: ShardOptions) {
     const shardSize = Math.ceil(tests.length / shardCount);
